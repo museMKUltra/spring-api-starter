6e6bf7a970149bed276cce899923ec960525b5d7
Roller Lo <jasonbh02@gmail.com>
2025-09-03 14:44:26 +0800

    feat: controller to get all products

---
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -0,0 +1,30 @@
+package com.codewithmosh.store.controllers;
+
+import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.mappers.ProductMapper;
+import com.codewithmosh.store.repositories.CategoryRepository;
+import com.codewithmosh.store.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
+
+    @GetMapping
+    public List<ProductDto> getProducts() {
+        return productRepository
+                .findAll()
+                .stream()
+                .map(productMapper::toDto)
+                .toList();
+    }
+}
