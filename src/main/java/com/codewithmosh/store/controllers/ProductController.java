e1455f7aa7fd024bcf57451080bf26efd4b3e674
Roller Lo <jasonbh02@gmail.com>
2025-09-03 14:57:59 +0800

    feat: get products by category id

--- src/main/java/com/codewithmosh/store/controllers/ProductController.java
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -1,12 +1,14 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.entities.Product;
 import com.codewithmosh.store.mappers.ProductMapper;
 import com.codewithmosh.store.repositories.CategoryRepository;
 import com.codewithmosh.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,11 +22,16 @@ public class ProductController {
     private final CategoryRepository categoryRepository;
 
     @GetMapping
-    public List<ProductDto> getProducts() {
-        return productRepository
-                .findAll()
-                .stream()
-                .map(productMapper::toDto)
-                .toList();
+    public List<ProductDto> getProducts(
+            @RequestParam(name = "categoryId", required = false) Byte categoryId
+    ) {
+        List<Product> products;
+        if (categoryId == null) {
+            products = productRepository.findAll();
+        } else {
+            products = productRepository.findByCategoryId(categoryId);
+        }
+
+        return products.stream().map(productMapper::toDto).toList();
     }
 }
