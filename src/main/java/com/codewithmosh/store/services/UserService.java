7fe6805a6a75580be3d216bac9f0cd809d41ea69
Roller Lo <jasonbh02@gmail.com>
2025-09-19 00:12:09 +0800

    refactor: working with authentication manager

---
+++ src/main/java/com/codewithmosh/store/services/UserService.java
@@ -0,0 +1,32 @@
+package com.codewithmosh.store.services;
+
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Collections;
+
+@AllArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("User not found"));
+        return new User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
