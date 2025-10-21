0d5f79ec57d7811941b6704f52fdc91f6d4526f7
Roller Lo <jasonbh02@gmail.com>
2025-10-21 16:29:53 +0800

    refactor(SecurityConfig): extract rules to feature layers

---
+++ src/main/java/com/codewithmosh/store/admin/AdminSecurityRules.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.store.admin;
+
+import com.codewithmosh.store.common.SecurityRules;
+import com.codewithmosh.store.users.Role;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+
+public class AdminSecurityRules implements SecurityRules {
+    @Override
+    public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry.requestMatchers("/admin/**").hasRole(Role.ADMIN.name());
+    }
+}
---
+++ src/main/java/com/codewithmosh/store/carts/CartSecurityRules.java
@@ -0,0 +1,12 @@
+package com.codewithmosh.store.carts;
+
+import com.codewithmosh.store.common.SecurityRules;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+
+public class CartSecurityRules implements SecurityRules {
+    @Override
+    public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry.requestMatchers("/carts/**").permitAll();
+    }
+}
