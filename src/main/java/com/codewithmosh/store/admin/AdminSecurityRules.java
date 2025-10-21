fcb17cf903493da9299115d8104f272dc2df77ba
Roller Lo <jasonbh02@gmail.com>
2025-10-21 22:56:29 +0800

    fix: beans of security rules

--- src/main/java/com/codewithmosh/store/admin/AdminSecurityRules.java
+++ src/main/java/com/codewithmosh/store/admin/AdminSecurityRules.java
@@ -4,7 +4,9 @@ import com.codewithmosh.store.common.SecurityRules;
 import com.codewithmosh.store.users.Role;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AdminSecurityRules implements SecurityRules {
     @Override
     public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
