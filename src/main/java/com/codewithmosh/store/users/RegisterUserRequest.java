b63e34d1d39dee9fa41e5a374a28046597bce57c
zebra <roller@rexlite.com.tw>
2026-03-31 11:45:46 +0800

    feat(users): enforce stronger password validation in registration
    - Increased minimum password length to 8 characters.
    - Required passwords to include at least one uppercase letter and one number.

--- src/main/java/com/codewithmosh/store/users/RegisterUserRequest.java
+++ src/main/java/com/codewithmosh/store/users/RegisterUserRequest.java
@@ -2,6 +2,7 @@ package com.codewithmosh.store.users;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -17,6 +18,10 @@ public class RegisterUserRequest {
     private String email;
 
     @NotBlank(message = "Password is required.")
-    @Size(min = 6, max = 25, message = "Password must be between 6 to 25 characters long.")
+    @Size(min = 8, max = 25, message = "Password must be between 8 and 25 characters long.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$",
+            message = "Password must contain at least one uppercase letter and one number."
+    )
     private String password;
 }
