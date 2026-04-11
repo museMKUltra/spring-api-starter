1b1fb7788f10d75d134a3f77382ed006e49cf097
Roller Lo <jasonbh02@gmail.com>
2026-04-11 15:43:19 +0800

    feat(Attendance): add global label support and enhance label query logic

--- src/main/java/com/codewithmosh/store/attendance/LabelDto.java
+++ src/main/java/com/codewithmosh/store/attendance/LabelDto.java
@@ -1,10 +1,19 @@
 package com.codewithmosh.store.attendance;
 
+import com.codewithmosh.store.users.User;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 public class LabelDto {
     private Long id;
     private String name;
     private String color;
+    @Getter(AccessLevel.NONE)
+    private User user;
+
+    public boolean getIsGlobal() {
+        return user == null;
+    }
 }
