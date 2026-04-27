ba0ed576f010b391b245c6dab0223ea8bb5ee82a
zebra <roller@rexlite.com.tw>
2026-04-27 17:06:33 +0800

    perf(attendance): optimize `findByToken` query with `@EntityGraph` to eagerly fetch associated user

--- src/main/java/com/codewithmosh/store/attendance/RefreshTokenRepository.java
+++ src/main/java/com/codewithmosh/store/attendance/RefreshTokenRepository.java
@@ -1,10 +1,12 @@
 package com.codewithmosh.store.attendance;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+    @EntityGraph(attributePaths = "user")
     Optional<RefreshToken> findByToken(String token);
 
     void deleteByToken(String token);
