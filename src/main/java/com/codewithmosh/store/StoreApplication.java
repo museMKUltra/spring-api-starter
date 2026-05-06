d2dc547149ea6627a35a88116cd50da84ba636c1
zebra <roller@rexlite.com.tw>
2026-05-06 17:23:24 +0800

    fix(store): enable Spring Data web support with DTO-based pagination

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -2,10 +2,14 @@ package com.codewithmosh.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class StoreApplication {
 
     public static void main(String[] args) {
